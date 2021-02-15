95437c6fc65cdf4873710bf2ce8053ec28975e0d
elocremarc <>
2021-02-15 00:14:01 -0800

    Sats table

--- src/BtcPrice.js
+++ src/BtcPrice.js
@@ -13,36 +13,94 @@ const BtcPrice = () => {
   const getPrice = async () => {
     const response = await fetch(url);
     const price = await response.json();
+
     let btcPrice = price.bpi.USD.rate_float;
     let btcRnd = Math.round(btcPrice);
+
     setPrice(commaNum(btcRnd));
-    priceSats = String(Math.round(100000000 / btcPrice));
-    setPriceSats(commaNum(priceSats));
+    priceSats = String(Math.round(sats / btcPrice));
+    setPriceSats(priceSats);
   };
 
   useEffect(() => {
     getPrice();
-    document.title = `$1 buys ${priceSats} Sats`;
+    document.title = `$1 buys ${commaNum(priceSats)} Sats`;
   });
+
+
+const tabledata = []
+
+const usbills = [1,5,10,20,100,1000]
+
+for (let i=0; i<usbills.length; i++) {
+  tabledata.push({qtySats: priceSats*usbills[i], qtyDolla: usbills[i]},)
+}
+
+const scale = 2;
+
+for (let i=usbills[usbills.length-1]*scale; priceSats*i<=sats; i=i*scale){
+
+tabledata.push({qtySats: priceSats*i, qtyDolla: i},)
+
+}
+
+tabledata.push({qtySats: sats, qtyDolla: price})
+
+
+const RenderTable = () => {
+return tabledata.map((data,index) => {
+  const {qtySats, qtyDolla,} = data
+  return (
+    <tr key={index}> 
+      <td>${commaNum(qtyDolla)}</td>
+      <td>{commaNum(qtySats)}</td>
+    </tr>
+  )
+
+})
+
+
+
+}
+
+
+
 
   return (
     <React.Fragment>
-      <div class="container h-100 ">
+      <div class="container">
         <div class="col-12 text-light text-center">
           <h1 class="display-4 text-center pt-4">Think Sats</h1>
-          <h5> $1 buys {priceSats} sats</h5>
+          <h5> $1 buys {commaNum(priceSats)} sats</h5>
         </div>
 
         <div class="d-flex justify-content-center">
           <p class="w-75 container pt-4 text-left text-light">
             A Bitcoin can be broken down into {commaNum(sats)} sats. Thinking in
             terms of sats is much more useful than looking at the bitcoin price.
-            Currently if you spend $1 it is like spending {priceSats} sats.
+            Currently if you spend $1 it is like spending {commaNum(priceSats)} sats.
             Bookmark this site and check back to calibrate as the price changes.
           </p>
         </div>
-        <div class="d-flex flex-column">
-          <div class="flex-grow-1 "></div>
+
+
+        <div class="d-flex p-4 w-75 justify-content-center container">
+          <div class="flex-grow-1 ">
+          <table class="table text-light table-sm ">
+          <thead>
+            <tr>
+            <th>Dollars</th>
+              <th>Sats</th>
+            </tr>
+          </thead>
+          <tbody>
+      <RenderTable/>
+          </tbody>
+        </table>
+          
+          
+          
+          </div>
         </div>
         <div class="col-sm-4"></div>
         <div
